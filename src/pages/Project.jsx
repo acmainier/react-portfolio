f70de0386154c4ea269728243a569ebc1fb80cc1
Anne-Cécile Mainier <annececile.mainier@gmail.com>
2026-07-25 17:05:09 +0100

    Display projects

---
+++ src/pages/Project.jsx
@@ -0,0 +1,8 @@
+export function Project({ name }) {
+  return (
+    <dl>
+      <dt>Name</dt>
+      <dd>{name}</dd>
+    </dl>
+  );
+}
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,3 +1,22 @@
+import { Project } from "./Project";
+
+const PROJECTS = [
+  { name: "Verdana Botanical Studio", key: "verdana" },
+  { name: "FloraFindr", key: "florafindr" },
+  { name: "Deployed ToDo List", key: "todolist" },
+];
+
 export function Projects() {
-  return <h1>Projects</h1>;
+  return (
+    <>
+      <h1>My projects</h1>
+      <ul>
+        {PROJECTS.map((item) => (
+          <li>
+            <Project name={item.name} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
